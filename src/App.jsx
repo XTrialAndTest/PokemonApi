ea957d6cc3c93a9f18be3ae89ab878e8cbfd5641
Xtrialandtest <1trialandtest@gmail.com>
2023-03-04 14:18:50 +0300

    first api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import Rick from './component/RickandMorty';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className='App'>
-      <div>
-        count is {count}
-        <div className='card'>
-          <button onClick={() => setCount((count) => count + 1)}>click</button>
-          <button onClick={() => setCount(0)}>reset</button>
-        </div>
-      </div>
+      <Rick />
     </div>
   );
 }
